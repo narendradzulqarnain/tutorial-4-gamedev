740b88c607a5951fbeb3fac5da8b7f44c7607eeb
narendradzulqarnain <narendra.dz1107@gmail.com>
2025-03-06 20:28:22 +0700

    fixed sprite direction

--- scenes/BlueSlime.gd
+++ scenes/BlueSlime.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var speed: float = 150.0
-@export var gravity: float = 500.0 
+@export var gravity: float = 500.0
 @export var sceneName: String = "LoseScene"
 
 @onready var sprite = $AnimatedSprite2D
@@ -18,9 +18,9 @@ func _physics_process(delta):
 func _process(_delta):
 	if velocity.x != 0:
 		if velocity.x > 0:
-			sprite.flip_h = false
-		else:
 			sprite.flip_h = true
+		else:
+			sprite.flip_h = false
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
